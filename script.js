2b56693b03a7919be8892f6db8f908d62cc5ffc2
Anirudhya <anirudhyaduyti@gmail.com>
2022-02-24 00:03:07 +0600

    detail

--- script.js
+++ script.js
@@ -8,10 +8,27 @@ getData();
 const showCountries = countries => {
     countries.forEach(country => {
         const countryDiv = document.createElement('div');
-        const h2 = document.createElement('h2');
-        h2.innerText = country.name.common;
-        countryDiv.appendChild(h2);
-        countryDiv.classList.add('country')
+        const img = document.createElement('img');
+        img.src = country.flags.png;
+        img.width = 80;
+        img.height = 40;
+        img.title = country.name.common;
+        countryDiv.appendChild(img);
+        countryDiv.classList.add('country');
         document.getElementById('countries').appendChild(countryDiv);
+        countryDiv.addEventListener('click', function () {
+            showCountry(country.name.common);
+        });
     })
+}
+const showCountry = name => {
+    fetch(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+        .then(response => response.json())
+        .then(data => showDetail(data[0]))
+}
+const showDetail = country => {
+    console.log(country);
+    const countryDetail = document.getElementById('country');
+    countryDetail.innerText = `${country.name.common} is in ${country.continents.join(' and ')} with population of ${country.population}. It's capital is ${country.capital[0]}`;
+    countryDetail.style.display = 'block';
 }
